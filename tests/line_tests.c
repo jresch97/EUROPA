f42badd09b8766ec60e4a53e14ece831c5f8d067
Jared Resch <jared.resch@hotmail.com>
2022-01-17 01:31:44 -0500

    update line tests

--- tests/line_tests.c
+++ tests/line_tests.c
@@ -24,83 +24,54 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include <time.h>
+#include <platform.h>
+#include <clock.h>
 #include <window.h>
-#include <unistd.h>
 
 #define WINCAP "EUROPA"
 #define WINW    640
 #define WINH    480
 #define WIND    32
 #define TGTFPS  60
-#define MSPERS  1000
-#define USPERS  1000000
-#define NSPERS  1000000000
-
-typedef struct timespec TIMESPEC;
-
-void deltatime(TIMESPEC *t1, TIMESPEC *t2, TIMESPEC *td)
-{
-        /* https://stackoverflow.com/a/53708448 */
-        td->tv_nsec = t2->tv_nsec - t1->tv_nsec;
-        td->tv_sec  = t2->tv_sec  - t1->tv_sec;
-        if (td->tv_sec > 0 && td->tv_nsec < 0) {
-                td->tv_nsec += NSPERS, td->tv_sec--;
-        }
-        else if (td->tv_sec < 0 && td->tv_nsec > 0) {
-                td->tv_nsec -= NSPERS, td->tv_sec++;
-        }
-}
 
 int main(int argc, char *argv[])
 {
         const WINSYS *sys;
         WINDOW       *win;
         SURFACE      *surf;
-        TIMESPEC      start, end, delta;
-        int           x, y, c, i, b, fps, tgtfps, showfps;
-        long long     dt, accum, a;
+        int           x, y, i, c, fps, fpsc, tfps;
+        long long     s, e, a;
         sys  = winsysd();
         wininit(sys);
         win  = winalloc(WINCAP, WINCTR, WINCTR, WINW, WINH, WIND, NULL);
         surf = winsurf(win);
-        i = fps = accum = 0, showfps = -1;
-        tgtfps = (argc > 1) ? atoi(argv[1]) : TGTFPS;
+        i    = fpsc = 0, a = 0, fps = -1;
+        tfps = argc > 1 ? atoi(argv[1]) : TGTFPS;
         while (winopen(win)) {
-                clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+                s = clkelapt();
                 for (y = 0; y < surf->h; y++) {
                         for (x = 0; x < surf->w; x++) {
-                                ((int*)surf->px)[x + y * surf->w] = rand();
+                                c = rand();
+                                ((int*)surf->px)[x + y * surf->w] = c;
                         }
                 }
                 winswap(win);
                 winpoll(sys);
-                fps++, i++;
-                if (showfps >= 0) {
-                        printf("fps=%d\n", showfps);
-                        showfps = -1;
+                if (fps >= 0) {
+                        printf("fps=%d\n", fps);
+                        fps = -1;
                 }
-                clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-                deltatime(&start, &end, &delta);
-                dt = (delta.tv_sec * NSPERS) + delta.tv_nsec;
-                if (tgtfps > 0) {
-                        a = (NSPERS / tgtfps) - dt;
-                        b = a / NSPERS;
-                        end.tv_sec  = b;
-                        end.tv_nsec = a - (b * NSPERS);
-                        if (end.tv_sec >= 0 && end.tv_nsec >= 0) {
-                                nanosleep(&end, NULL);
-                        }
+                e = clkelapt();
+                if (tfps > 0) {
+                        clkslept((clkfreq() / tfps) - (e - s));
                 }
-                clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-                deltatime(&start, &end, &delta);
-                dt = (delta.tv_sec * NSPERS) + delta.tv_nsec;
-                accum += dt;
-                if (accum >= NSPERS) {
-                        showfps = fps;
-                        accum   = 0;
-                        fps     = 0;
+                e = clkelapt();
+                a += (e - s);
+                if (a >= clkfreq()) {
+                        fps = fpsc;
+                        a   = fpsc = 0;
                 }
+                fpsc++, i++;
         }
         winfree(win);
         winterm(sys);
